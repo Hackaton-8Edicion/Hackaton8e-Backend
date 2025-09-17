835e2bdcebb256963af3d445876573da8d8fb975
paolap <paopin2024@gmail.com>
2025-09-17 12:17:34 +0200

    first commit

---
+++ src/test/java/hackaton8e_sanitas/group8/Group8ApplicationTests.java
@@ -0,0 +1,13 @@
+package hackaton8e_sanitas.group8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
