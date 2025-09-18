7527b56db85a1fd63cf01d276ddfad1ae7baebf2
Ivan Lorenzo <elhibar77@hotmail.es>
2025-09-18 14:31:57 +0200

    feat: user mapper implemented

---
+++ src/main/java/hackaton8e_sanitas/group8/user/dtos/user/UserMapper.java
@@ -0,0 +1,22 @@
+package hackaton8e_sanitas.group8.user.dtos.user;
+
+public class UserMapper {
+
+    public static UserEntity toEntity(UserRequestDTO dto) {
+        if (dto == null) return null;
+        return UserEntity.builder()
+                .name(dto.name())
+                .username(dto.username())
+                .email(dto.email())
+                .build();
+    }
+
+    public static UserRequestDTO toRequestDto(UserEntity entity) {
+        if (entity == null) return null;
+        return new UserRequestDTO(
+                entity.getName(),
+                entity.getUsername(),
+                entity.getEmail()
+        );
+    }
+}
